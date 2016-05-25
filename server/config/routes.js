9b24964d6cd678dfa71ac5cd59f6a54e7a5a623c
David Chen <chentiki808@gmail.com>
2016-05-24 17:01:45 -0700

    first commit

---
+++ server/config/routes.js
@@ -0,0 +1,24 @@
+// ROUTES JS
+
+// require tweets controller
+
+// Uncomment the following line when ready to create project!
+// var tweets = require('./../controllers/tweets.js');
+
+
+
+module.exports = function(app){
+
+	// GETS ==================================
+	
+	// app.get('/get_Tweets', function(req, res){
+	// 	tweets.getTweets(req, res);
+	// });
+
+
+	// POSTS =================================
+
+
+
+
+}
